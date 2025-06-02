50cda483faccb629e9bbfa3a2a5a6225a35a9efd
QaisarGit <qaisar.fareed@gmail.com>
2025-06-02 18:19:04 +0530

    first without local repo

---
+++ src/test/java/NewEclipsePacjkage/NewEclipsePackClass.java
@@ -0,0 +1,12 @@
+package NewEclipsePacjkage;
+
+import org.testng.annotations.Test;
+
+public class NewEclipsePackClass {
+
+	@Test
+	public void NewEclipsePackClassMethod() {
+		System.out.println("New eclipse method --- steps");
+		
+	}
+}
