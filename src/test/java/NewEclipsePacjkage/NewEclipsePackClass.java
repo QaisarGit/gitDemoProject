e2f084dfda0748541c010055ccdd7e39f61280e4
QaisarGit <qaisar.fareed@gmail.com>
2025-06-03 11:07:20 +0530

    commit 3-6-3

--- src/test/java/NewEclipsePacjkage/NewEclipsePackClass.java
+++ src/test/java/NewEclipsePacjkage/NewEclipsePackClass.java
@@ -9,7 +9,9 @@ public class NewEclipsePackClass {
 		System.out.println("New eclipse method --- steps");
 		System.out.println("New eclipse method --- steps");
 
-	
+		System.out.println("New eclipse method --- steps");
+		System.out.println("New eclipse method --- steps");
+
 		
 
 		
